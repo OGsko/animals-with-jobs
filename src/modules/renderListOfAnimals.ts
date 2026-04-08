e7e2af2c7f89ba7741721357eac65db6d86d1d04
oskar <oskarl1@live.com>
2026-04-08 16:37:33 +0200

    All functionality is working. Styling leftgit status

--- src/modules/renderListOfAnimals.ts
+++ src/modules/renderListOfAnimals.ts
@@ -3,4 +3,19 @@
 //Varje list-item ska ha en event-lyssnare som kallar på "renderAnimalInfo"
 //Typa upp funktionens parameter
 
-export default function renderListOfAnimals(animals) {}
+import { Animal } from "./IAnimal.ts";
+import renderAnimalInfo from "./renderAnimalInfo.ts"
+
+export default function renderListOfAnimals(animal: Animal[]) {
+    const animalNameList = document.querySelector(".list-of-animals ul") as HTMLElement
+
+    animal.forEach((animalName: Animal) => {
+        const li = document.createElement("li")
+        li.textContent = animalName.name
+        animalNameList.appendChild(li)
+        li.addEventListener("click", () => {
+            renderAnimalInfo(animalName)    
+        })
+        
+    });
+}
